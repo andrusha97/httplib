efcab75cea108e42dbf2561ceb99381c5f049352
Andrey Goryachev <andrey.goryachev@gmail.com>
2017-02-24 23:22:24 +0300

    One more test.

---
+++ tests/http/request.cpp
@@ -0,0 +1,34 @@
+#include <catch.hpp>
+
+#include <httplib/http/request.hpp>
+
+#include <sstream>
+
+
+TEST_CASE("request's output operator outputs", "[http_request_t]") {
+    httplib::http_request_t request {
+        "MyMETHOD",
+        "/url/path?query&arg=value#frag",
+        {13, 37},
+        {
+            {"Content-Length", {"10"}},
+            {"Content-Type", {"application/json"}},
+            {"Home", {"localhost"}},
+            {"xxx", {"yyy", "zzz"}}
+        }
+    };
+
+    std::ostringstream stream;
+    stream << request;
+
+    std::string expected =
+        "MyMETHOD /url/path?query&arg=value#frag HTTP/13.37\r\n"
+        "Home: localhost\r\n"
+        "Content-Length: 10\r\n"
+        "Content-Type: application/json\r\n"
+        "xxx: yyy\r\n"
+        "xxx: zzz\r\n"
+        "\r\n";
+
+    REQUIRE(stream.str() == expected);
+}
